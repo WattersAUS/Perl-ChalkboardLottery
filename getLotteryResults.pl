2b359b556f8fb02240e3a474a40fa77b750f91ef
Graham Watson <grahamj.watson@gmail.com>
2016-11-30 18:54:36 +0000

    Now with added validate and dummy database writes added

--- getLotteryResults.pl
+++ getLotteryResults.pl
@@ -9,12 +9,13 @@
 #	2016-11-21	v0.01		First cut of code
 #	2016-11-27	v0.02		First attempt at basic functionality
 #	2016-11-28	v0.03		Moved logger write to sqlBuffer
+#	2016-11-30	v0.04		Added a WorkSpace directory for file processing
 #
 
 use lib "./Classes";
 
 use strict;
-use vars qw($optDebug $optEmail $optHelp $optMailServer $optInsert $optTimestamp);
+use vars qw($opt_d $opt_e $opt_h $opt_m $opt_i $opt_t $opt_w);
 use Getopt::Std;
 use DBI;
 use File::Basename;
@@ -25,6 +26,7 @@ use MIME::Lite;
 #-----------------------------------------------------------------------------
 
 use Lottery::draw_history;
+use Lottery::logger;
 use Lottery::lottery_draws;
 use Lottery::number_usage;
 
@@ -32,7 +34,7 @@ use Lottery::number_usage;
 # only globals in the whole program (I hope)
 #-----------------------------------------------------------------------------
 
-my $version_id = "0.03";
+my $version_id = "0.04";
 
 #-----------------------------------------------------------------------------
 # this holds sql statements batched up (bit like a transaction for each line)
@@ -60,18 +62,29 @@ sub debugHelpMessage {
 }
 
 #-----------------------------------------------------------------------------
+# log messages to the database and to stdout via debugMessage
+#-----------------------------------------------------------------------------
+
+sub logMessage {
+    my $message = shift;
+    debugMessage($message);
+    buildLogger($message);
+    return;
+}
+
+#-----------------------------------------------------------------------------
 # print out DEBUG messages allowing for possible date/time stamp inclusion
 #-----------------------------------------------------------------------------
 
 sub debugMessage {
     my $debugMsg = shift;
 	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
-    if ($optTimestamp) {
+    if ($opt_t) {
 		printf "[ %4d-%02d-%02d %02d:%02d:%02d ] ", $year+1900,$mon+1,$mday,$hour,$min,$sec;
     }
     print "DEBUG: ";
     if (defined($debugMsg)) {
-            print $debugMsg;
+        print $debugMsg;
     }
     print "\n";
     return;
@@ -88,6 +101,7 @@ sub buildNewDrawHistory {
 	$drawHistory->draw($draw);
 	$drawHistory->draw_date($drawDate);
 	$drawHistory->CreateINSERT;
+    debugMessage($drawHistory->{SQL_STATEMENT}->[0]);
 	push(@sqlBuffer, $drawHistory->{SQL_STATEMENT}->[0]);
 	return;
 }
@@ -107,6 +121,7 @@ sub buildNewNumberUsage {
 	$numberUsage->number($number);
 	$numberUsage->is_special($isSpecial);
 	$numberUsage->CreateINSERT;
+    debugMessage($numberUsage->{SQL_STATEMENT}->[0]);
 	push(@sqlBuffer, $numberUsage->{SQL_STATEMENT}->[0]);
 	return;
 }
@@ -115,14 +130,13 @@ sub buildNewNumberUsage {
 # write to logger table
 #-----------------------------------------------------------------------------
 
-sub writeToLogger {
-	my $dbHandle = shift;
+sub buildLogger {
 	my $message  = shift;
 	my $logger   = new logger;
-	$logger->logger_description($message);
-	$logger->logger_timestamp("now()");
+	$logger->description($message);
+	$logger->last_modified("now()");
 	$logger->CreateINSERT;
-	debugMessage($logger->{SQL_STATEMENT}->[0]) if ($optDebug);
+	debugMessage($logger->{SQL_STATEMENT}->[0]);
     push(@sqlBuffer, $logger->{SQL_STATEMENT}->[0]);
 	return;
 }
@@ -132,17 +146,17 @@ sub writeToLogger {
 #-----------------------------------------------------------------------------
 
 sub sendEmailMessage {
-	my $fromWho = shift;
-	my $toWhom  = shift;
-	my $subject = shift;
-	my $body    = shift;
-	debugMessage("Sending mail message to ".$toWhom." (".$subject.")") if ($optDebug);
+	my $fromWhom = shift;
+	my $toWhom   = shift;
+	my $subject  = shift;
+	my $body     = shift;
+	debugMessage("Sending mail message to ".$toWhom." (".$subject.")");
 	my $mailMsg = MIME::Lite->new(
 			From	=>	$fromWhom,
 			To		=>	$toWhom,
 			Subject	=>	$subject,
 			Type	=>	'multipart/mixed'
-	) or debugMessage("Failed to send mail message to ".$toWhom." (".$subject.")") if ($optDebug);
+	) or debugMessage("Failed to send mail message to ".$toWhom." (".$subject.")");
 	$mailMsg->attach(
 			Type	=>	'text/plain',
 			Data	=>	$body
@@ -159,35 +173,130 @@ sub sendEmailMessage {
 sub getResultsPage {
     my $url  = shift;
     my $file = shift;
-    my $rc = system("wget", $url, , "-O", $file);
-    die "system() call to execute 'wget' failed with status ".$rc unless $rc == 0;
+
+    # remove the file in case we has already downloaded it
+    if (-e $file) {
+        unlink($file);
+    }
+
+    # go get it!
+    my @args = ("wget", $url, "-O", $file);
+    system(@args) == 0
+        or die "system() call to execute 'wget' failed with status ".$?;
     return;
 }
 
 #-----------------------------------------------------------------------------
-# parse the file that will contain the lottery results and pass back to caller
+# load the file into an array we can easily parse when we need
 #-----------------------------------------------------------------------------
 
-sub extractResultsFromPage {
-    my $draws = shift;
+sub loadResultsIntoArray {
     my $file  = shift;
+    # load the file into an array where we parse through multiple times
+    my @fileContents = qw();
+    open(INPUT, "< ".$file)
+        or die "Cannot results page ".$file." $!\n";
+    while (my $input_line = <INPUT>) {
+        push(@fileContents, $input_line);
+    }
+    close(INPUT);
+    return @fileContents;
+}
 
-    # hold results here
+#-----------------------------------------------------------------------------
+# parse the array stripping out the numbers or specials
+#-----------------------------------------------------------------------------
 
-    my @numbers = qw();
-    my @special = qw();
-    my $date    = "";
+sub extractFromArray {
+    my $numbers = shift;
+    my $tag     = shift;
+    my @data    = @_;
+    my @extracted = qw();
+    logMessage("Searching for tag ".$tag." in results page ...");
+    foreach my $line (@data) {
+        if ($line =~ m/$tag/) {
+            $line =~ s/.*$tag//;
+            $line =~ tr/0-9//cd;
+            push(@extracted, $line);
+        }
+    }
+    return (@extracted);
+}
 
-    # for the draw we are processing we need to know numbers/specials
+#-----------------------------------------------------------------------------
+# parse the array to extract the data string
+#
+# first find it
+# then strip out any lead spaces
+# finally the html tags
+#-----------------------------------------------------------------------------
 
-    my $numbers     = $draws->{numbers}->[0];
-    my $numbersTag  = $draws->{description}->[0];
-    my $specials    = $draws->{specials}->[0];
-    my $specialsTag = $draws->{specials_tag}->[0];
+sub extractDateFromArray {
+    my @data = @_;
+    foreach my $line (@data) {
+        if ($line =~ m/<h1>[MTWTFD][ouehra][meduitn].*20[12][1-9]<\/h1>/) {
+            $line =~ s/^[ \t]*//;
+            $line =~ s/<[\/]{0,1}h1>//g;
+            $line =~ s/\n//g;
+            $line =~ s/\r//g;
+            return $line;
+        }
+    }
+    return "";
+}
 
-    # start extracting
+#-----------------------------------------------------------------------------
+# check the numbers / specials we extracted are valid
+#
+# right number of results
+# within range
+#-----------------------------------------------------------------------------
 
-    return (@numbers, @special, $date);
+sub checkNumbersWithinLimits {
+    my $numbers = shift;
+    my $upper   = shift;
+    my $type    = shift;
+    my @array   = @_;
+    my $passed  = 1;
+
+    # have we got the right number of values
+    if ($numbers != ($#array + 1)) {
+        logMessage("ERROR: Loaded ".($#array + 1)." ".$type."s this does not match the expected value of ".$numbers."...");
+        $passed = 0;
+    }
+
+    # are they within range
+    foreach my $num (@array) {
+        if (($num < 1) || ($num > $upper)) {
+            logMessage("ERROR: The ".$type." ".$num." is not within the expected range of values 1 to ".$upper."...");
+            $passed = 0;
+        }
+    }
+    return $passed;
+}
+
+#-----------------------------------------------------------------------------
+# store the details of the draw and update the lottery
+#-----------------------------------------------------------------------------
+
+sub storeNumberUsage {
+
+    logMessage("Storing Number Usage...");
+
+    return;
+}
+
+sub storeDrawHistory {
+
+    logMessage("Storing Draw History...");
+
+    return;
+}
+
+sub updateLotteryDraw {
+    logMessage("Updating Lottery Draw...");
+
+    return;
 }
 
 #-----------------------------------------------------------------------------
@@ -199,38 +308,66 @@ sub extractResultsFromPage {
 
 sub processLotteryDraws {
     my $dbHandle = shift;
+    my $wrkSpace = shift;
 
-# iterate through configured lottery draws, we will get the 'next' draw
-# as we expect to already have the draw set in the db
-
+    # iterate through configured lottery draws, we will get the 'next' draw
+    # as we expect to already have the draw set in the db
     my $draws = new lottery_draws;
     $draws->ResetKEYFIELDS;
     $draws->DataSAVE;
     $draws->CreateSELECT;
-    debugMessage($draws->{SQL_STATEMENT}->[0]) if ($optDebug);
+    debugMessage($draws->{SQL_STATEMENT}->[0]);
     my $sth = $dbHandle->prepare($draws->{SQL_STATEMENT}->[0]);
     $sth->execute;
     while (my @fields = $sth->fetchrow) {
         $draws->DataINITIALISE(@fields);
         $draws->DataSAVE;
+        my $identifier   = $draws->{ident}->[0];
         my $description  = $draws->{description}->[0];
         my $draw         = $draws->{draw}->[0];
         my $base_url     = $draws->{base_url}->[0];
+        my $numbers      = $draws->{numbers}->[0];
+        my $upperNumber  = $draws->{upper_number}->[0];
+        my $numbersTag   = $draws->{numbers_tag}->[0];
+        my $specials     = $draws->{specials}->[0];
+        my $upperSpecial = $draws->{upper_special}->[0];
+        my $specialsTag  = $draws->{specials_tag}->[0];
 
-# now we need to prep settings to get the page for the next draw, using the url and filename
-
+        # now we need to prep settings to get the page for the next draw, using the url and filename
         $draw++;
         my $filename     = lc($description);
         $filename        =~ s/\s/_/g;
         $filename        .= ".".$draw;
         my $url          = $base_url;
         $url             =~ s/DRAWNUMBER/$draw/g;
-        debugMessage("Ready to process (".$draws->{description}->[0]."), draw (".$draw."), into file (".$filename."), using url (".$url.")") if ($optDebug);
-        getResultsPage($url, $filename)
-        if ! -f $filename {
-            debugMessage("ERROR: Unable to find file (".$filename.") to process...\n");
+
+        # get the results page itself
+        logMessage("Ready to get Lottery results for (".$draws->{description}->[0]."), draw (".$draw."), into file (".$filename."), using url (".$url.")");
+        my $downloadFile = $wrkSpace."/".$filename;
+        getResultsPage($url, $downloadFile);
+
+        # check we got a file, extract the numbers / specials / date and validate
+        if (! -e $downloadFile) {
+            logMessage("ERROR: Unable to find file (".$downloadFile.") to process...\n");
         } else {
-            my (@resultNumbers, @specialNumbers, $drawDate) = extractResultsFromPage($draws, $filename);
+            my @data = loadResultsIntoArray($downloadFile);
+            logMessage("Loaded results page, ".$#data." lines into an array for processing...");
+            my @resultNumbers  = extractFromArray($numbers,  $numbersTag, @data);
+            my @specialNumbers = extractFromArray($specials, $specialsTag, @data);
+            my $dateStr        = extractDateFromArray(@data);
+            if (checkNumbersWithinLimits($numbers, $upperNumber, "number", @resultNumbers) == 1) {
+                if (checkNumbersWithinLimits($specials, $upperSpecial, "special", @specialNumbers) == 1) {
+                    # make sure we pass all checks before we decide to store the values
+                    if (($#resultNumbers + 1) > 0) {
+                        storeNumberUsage($identifier, $draw, $numbers, 0, @resultNumbers);
+                        if (($#specialNumbers + 1) > 0) {
+                            storeNumberUsage($identifier, $draw, $specials, 1, @specialNumbers);
+                        }
+                        storeDrawHistory($identifier, $draw, $dateStr);
+                        updateLotteryDraw($identifier, $draw);
+                    }
+                }
+            }
         }
     }
     $sth->finish;
@@ -248,8 +385,7 @@ sub processSQLBuffer {
 	my $dbHandle = shift;
     my $count    = 0;
 	foreach my $sql_statement (@sqlBuffer) {
-		debugMessage($sql_statement) if ($optDebug);
-		if ($optInsert) {
+		if ($opt_i) {
 			my $sth = $dbHandle->prepare($sql_statement);
 			$sth->execute;
             $count++;
@@ -268,46 +404,53 @@ my $user = "shinyide2_ro";
 my $pass = "R3l9c675";
 my $host = "10.169.0.121";
 
+my $wrksp = "./";
+
 #-----------------------------------------------------------------------------
 # check for any command line params and set appropriately
 #-----------------------------------------------------------------------------
 
-getopts('dehim:t');
+getopts('dehim:tw:');
 
 # help screen
-
-if (defined($optHelp)) {
-	debugHelpMessage("\n\n\tLottery Data Upload Utility ".$version_id);
+if (defined($opt_h)) {
+	debugHelpMessage("\n\tLottery Data Upload Utility ".$version_id);
 	debugHelpMessage("\t================================");
 	debugHelpMessage( "\t-i (Activate Writing to DB)");
 	debugHelpMessage( "\t-h (view this HELP Mode)");
 	debugHelpMessage( "\t-d (turn on DEBUG Mode)");
 	debugHelpMessage( "\t-e (turn on EMAIL Mode)");
 	debugHelpMessage( "\t-t (turn on date/time stamp on DEBUG messages)");
-	debugHelpMessage( "\t-m <SMTP-server> (use this SMTP address and not SENDMAIL)\n\n");
+	debugHelpMessage( "\t-m <SMTP-server> (use this SMTP address and not SENDMAIL)");
+    debugHelpMessage( "\t-w <DIR>         (workspace directory to use)\n");
 	exit(0);
 }
 
 # date/time stamp on DEBUG messages
-
-if (defined($optTimestamp)) {
-	debugMessage("Date/Time Stamp Active") if ($optDebug);
+if (defined($opt_t)) {
+	debugMessage("Date/Time Stamp Active");
 } else {
-    debugMessage("Date/Time Stamp Inactive") if ($optDebug);
+    debugMessage("Date/Time Stamp Inactive");
 }
 
 # email mode
-
-if (defined($optEmail)) {
-	debugMessage("Email Sending Active") if ($optDebug);
+if (defined($opt_e)) {
+	debugMessage("Email Sending Active");
 } else {
-	debugMessage("Email Sending Inactive") if ($optDebug);
+	debugMessage("Email Sending Inactive");
 }
 
-if (defined($optMailServer)) {
-	debugMessage("Mail Server: ".$opt_m) if ($optDebug);
+# define a mail server if needed
+if (defined($opt_m)) {
+	debugMessage("Mail Server: ".$opt_m);
 	MIME::Lite->send('smtp', $opt_m, Timeout=>60);
 }
+
+# work directory
+if (defined($opt_w)) {
+	$wrksp = $opt_w;
+}
+debugMessage(" Work Space: ".$wrksp);
 
 #-----------------------------------------------------------------------------
 # main code
@@ -315,20 +458,18 @@ if (defined($optMailServer)) {
 
 my $dbHandle = DBI->connect("DBI:mysql:".$db.":".$host, $user, $pass);
 if (! $dbHandle) {
-    debugMessage("Can't get access to the database\n") if ($optDebug);
+    debugMessage("Can't get access to the database\n");
 } else {
 
     # opened the db successfully, so now we can process the draws
-
-    debugMessage("Querying database for draws to process...") if ($optDebug);
-    processLotteryDraws($dbHandle);
+    debugMessage("Querying database for draws to process...");
+    processLotteryDraws($dbHandle, $wrksp);
 
     # done, close the db and get out of here!
-
     $dbHandle->disconnect();
-    debugMessage("Closed database connection...") if ($optDebug);
+    debugMessage("Closed database connection...");
 }
 
-debugMessage("End of line...") if ($optDebug);
+debugMessage("End of line...");
 
 # End of Line.....
